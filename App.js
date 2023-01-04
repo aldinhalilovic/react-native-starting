b308280d5bdec51c4c5b5d471ed65fcaa3497a32
aldinhalilovic <aldinhalilovic06@gmail.com>
2023-01-04 15:10:43 +0100

    spacing

--- App.js
+++ App.js
@@ -2,33 +2,42 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Button
-        title="Click me!"
-        onPress={() => console.log("first")}
-        color={"black"}
-      />
-      <Text
-        style={{
-          borderWidth: 2,
-          borderColor: "blue",
-          margin: 12,
-          padding: 12,
-        }}
-      >
-        dslkfjdslkfj
-      </Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your goal" style={styles.textInput} />
+        <Button title="Add Goal" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
     height: "100%",
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    borderRadius: 10,
+    width: "70%",
+    padding: 8,
+    marginRight: 8,
+  },
+  goalsContainer: {
+    flex: 3,
   },
 });
